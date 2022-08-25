d245b5acbfc912cb438a071764c5777d59965080
budch4nin <pashoo0ook@mail.ru>
2022-08-25 17:16:52 +0300

    useEffect cleanup

--- src/stories/UseEffect.stories.tsx
+++ src/stories/UseEffect.stories.tsx
@@ -6,22 +6,96 @@ export default {
 }
 
 
-export const SetTimeoutExample = () =>{
+export const SetIntervalExample = () =>{
     const [counter, setCounter] = useState(1)
 
     console.log('SetTimeoutExample')
 
-    // useEffect(()=>{
-    //
-    //     setInterval(()=>{
-    //         setCounter(state => state + 1)
-    //     }, 1000)
-    //
-    // }, [])
+    useEffect(()=>{
+
+        const intId = setInterval(()=>{
+            setCounter(state => state + 1)
+        }, 1000)
+
+        return () => {
+            clearInterval(intId)
+        }
+
+    }, [])
 
 
 
     return <>
     Hello, counter: {counter}
     </>
+}
+
+export const ResetEffectExample = () => {
+
+    const [counter, setCounter] = useState(0)
+
+    console.log('component rendered ' + counter)
+
+    useEffect(() => {
+        console.log('useEffect ' + counter)
+
+        return () => {
+            console.log('resetEffect ' + counter)
+        }
+    }, [counter])
+
+    const onClickHandler = () => {
+        setCounter(counter + 1)
+    }
+
+    return <>
+        {counter}<button onClick={onClickHandler}>+</button>
+    </>
+
+}
+
+
+export const KeysTrackerExample = () => {
+
+    const [text, setText] = useState('')
+
+    console.log(text)
+
+    useEffect(() => {
+        const handler = (e: KeyboardEvent) => {
+            console.log(e.key)
+            setText(text + e.key)
+        }
+
+        window.addEventListener('keypress', handler)
+        return () => {
+            window.removeEventListener('keypress', handler)
+        }
+    }, [text])
+
+
+    return <div>
+        {text}
+    </div>
+}
+
+
+export const SetTimeoutExample = () => {
+
+    console.log('component rendered')
+
+    const [text, setText] = useState('')
+
+    useEffect(() => {
+        const toID = setTimeout(() => {
+            console.log('my name is timeout')
+            setText('Hello')
+        }, 3000)
+
+        return () => {
+            clearInterval(toID)
+        }
+    }, [text])
+
+    return <div>{text}</div>
 }
